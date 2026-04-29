7b2049413a9f7f679d054513b2116b09c6e1c408
PiyamitMARK <zzmarkzz1329@gmail.com>
2026-04-29 18:00:30 +0700

    add menu

--- app.js
+++ app.js
@@ -65,15 +65,7 @@ const products = {
     { id: 'kao8', name: 'ข้าวเหนียว',           price: 10, image: IMG(10002),  productType: 'simple' },
     { id: 'kao9', name: 'ข้าวสวย',           price: 10, image: IMG(10003),  productType: 'simple' },
     { id: 'kao6', name: 'ไข่ต้ม',               price: 10, image: IMG(1090),  productType: 'simple' },
-  ],
-  kao: [
-    { id: 'kao1', name: 'ข้าวซอยน่องไก่', price: 70, image: IMG(111),   productType: 'kaosoi'   },
-    { id: 'kao2', name: 'ข้าวซอยหมูทอด',       price: 70, image: IMG(1007),   productType: 'kaosoi'  },
-    { id: 'kao3', name: 'น้ำเงี้ยว',            price: 60, image: IMG(555),    productType: 'namngiao'  },
-    { id: 'kao4', name: 'ข้าวหมูทอด',           price: 50, image: IMG(7667),   productType: 'kaomutod'  },
-    { id: 'kao5', name: 'แคบหมู',               price: 15, image: IMG(98789),  },
-    { id: 'kao6', name: 'ไข่ต้ม',               price: 10, image: IMG(1090),  },
-  ],
+    ],
   nam: [
     { id: 'nam1',  name: 'น้ำเปล่า',       price: 10, image: IMG(60),  productType: 'drink-ready' },
     { id: 'nam2',  name: 'โค๊ก',           price: 15, image: IMG(80),  productType: 'drink-ready' },
